3d4228573bc1cbd6b1fcff5bd9910106a92362d7
xfrnk2 <xfrnk2@gmail.com>
2022-02-22 23:46:54 +0900

    자바 백준 2908 풀이

---
+++ src/baekjun/BOJ2908/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+        sc.close();
+        
+		
+		System.out.print(A > B ? A : B);
+	
+	}
+}
