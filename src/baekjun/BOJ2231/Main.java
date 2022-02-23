0a3320ac0e31d4ab7b3bf981ca9132ad029bc211
xfrnk2 <xfrnk2@gmail.com>
2022-02-23 22:24:03 +0900

    자바 백준 2231 풀이 (brute force)

---
+++ src/baekjun/BOJ2231/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        
+        int answer = search(N);
+        System.out.print(answer);
+    }
+    
+    static int search(final int N){
+        for (int i = 1; i < N; i++){
+            int temp = i;
+            int result = 0;
+            while (temp > 0){
+                result += temp % 10;
+                temp /= 10;
+            }
+            if (result + i == N){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
