d238a7ccb02baef85c5028c91ab6c6f52ddc2c40
xfrnk2 <xfrnk2@gmail.com>
2022-02-23 22:23:44 +0900

    자바 백준 2908 풀이 (brute force)

---
+++ src/baekjun/BOJ2798/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
+        
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++){
+            numbers[i] = in.nextInt();
+        } 
+        int answer = search(numbers, N, M);
+        System.out.println(answer);
+    }
+    
+    static int search (final int[] numbers, final int N, final int M) {
+        int result = 0;
+        for (int i = 0; i < N - 2; i++){
+            for (int j = i + 1; j < N - 1; j++){
+                for (int k = j + 1; k < N; k++){
+                    int temp = numbers[i] + numbers[j] + numbers[k];
+                    
+                    if (temp == M){
+                        return temp;
+                    }
+					if(result < temp && temp < M) {
+						result = temp;
+					}
+                }
+            }
+        }
+        return result;
+    }
+}
