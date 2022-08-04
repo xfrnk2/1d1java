a3852c56d538ceb5fd93c093d44a9718154a82fe
xfrnk2 <xfrnk2@gmail.com>
2022-08-04 20:19:49 +0900

    refactor: to use BufferedWriter

--- src/main/java/boj/p2k/P2100/P2164.java
+++ src/main/java/boj/p2k/P2100/P2164.java
@@ -1,17 +1,23 @@
 package boj.p2k.P2100;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class P2164 {
-	public static void main(String[] args) {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
+
 		int n = sc.nextInt();
 		solution(n);
+		bw.flush();
 	}
 
-	public static void solution(int n) {
+	public static void solution(int n) throws IOException {
 		Queue<Integer> queue = new LinkedList<>();
 		for (int i = 1; i <= n; i++) {
 			queue.offer(i);
@@ -21,6 +27,7 @@ public class P2164 {
 			queue.poll();
 			queue.offer(queue.poll());
 		}
-		System.out.print(queue.poll());
+		bw.write(queue.poll() + "");
+		System.out.println((char)6);
 	}
 }
