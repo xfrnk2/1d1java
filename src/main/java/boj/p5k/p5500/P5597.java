61f1f2918af84f7a4009a6dba7a5635ded2f8903
xfrnk2 <xfrnk2@gmail.com>
2022-08-06 23:52:10 +0900

    solve: 5597 과제 안내신 분...?:

---
+++ src/main/java/boj/p5k/p5500/P5597.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		boolean[] attendance = new boolean[31];
+		for (int i = 0; i < 28; i++) {
+			attendance[Integer.parseInt(sc.nextLine())] = true;
+		}
+		for (int i = 1; i < 31; i++) {
+			if (!attendance[i]) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
