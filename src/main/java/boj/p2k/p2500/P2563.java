3e5fe37bfd2619c078d48a4bd16e2335a6b57c5f
xfrnk2 <xfrnk2@gmail.com>
2022-08-09 20:52:59 +0900

    solve: 2563 색종이

---
+++ src/main/java/boj/p2k/p2500/P2563.java
@@ -0,0 +1,24 @@
+package boj.p2k.p2500;
+import java.util.Scanner;
+
+public class P2563 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean[][] paper = new boolean[100][100]; //도화지 면적은 가로세로100
+		int n = sc.nextInt();
+		int ans = 0;
+		for(int k = 0; k < n; k++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			for(int i = x, xEnd = x + 10; i < xEnd; i++ ) {
+				for(int j = y, yEnd = y + 10; j < yEnd; j++) {
+					if(paper[i][j]) continue;
+					paper[i][j] = true;
+					ans++;
+				}
+			}
+		}
+		System.out.print(ans);
+	}
+}
