73b11106a744b368f4c4998a35ba78c98ea61fc2
xfrnk2 <xfrnk2@gmail.com>
2022-08-10 22:28:11 +0900

    solve: 2468 안전 영역

---
+++ src/main/java/boj/p2k/p2400/P2468.java
@@ -0,0 +1,69 @@
+package boj.p2k.p2400;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P2468 {
+	static int N;
+	static int[][] di = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	static int[][] area;
+	static boolean[][] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		int top = 0;
+		area = new int[N][N];
+		visited = new boolean[N][N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				int cur = Integer.parseInt(st.nextToken());
+				top = Math.max(top, cur);
+				area[i][j] = cur;
+			}
+		}
+		solution(N, top);
+	}
+
+	public static void search(int x, int y) {
+		visited[x][y] = true;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + di[i][0], ny = y + di[i][1];
+			if (nx < 0 || ny < 0 || N <= nx || N <= ny || visited[nx][ny] || area[nx][ny] <= 0)
+				continue;
+			search(nx, ny);
+		}
+	}
+
+	public static void solution(int n, int top) {
+
+		int ans = 1;
+
+		for (int r = 0; r < top; r++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					area[i][j]--;
+				}
+
+			}
+
+			int cnt = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (!visited[i][j] && area[i][j] > 0) {
+						search(i, j);
+						cnt += 1;
+					}
+				}
+			}
+			visited = new boolean[N][N];
+			ans = Math.max(ans, cnt);
+		}
+		System.out.println(ans);
+	}
+
+}
