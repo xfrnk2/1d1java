890c6b49fdcfabb9c284aa24c6f9854498abf6e2
xfrnk2 <xfrnk2@gmail.com>
2022-09-13 23:45:22 +0900

    solve: 2558 A + B 2

---
+++ src/main/java/boj/p2k/p2500/P2558.java
@@ -0,0 +1,14 @@
+package boj.p2k.p2500;
+import java.util.Scanner;
+
+public class P2558 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a + b);
+	}
+}
