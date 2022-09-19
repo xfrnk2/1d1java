b2b1bd146ba625a0d2a086d0404a7d19fb1d9007
xfrnk2 <xfrnk2@gmail.com>
2022-09-19 20:15:05 +0900

    refactor: 16724 피리 부는 사나이

--- src/main/java/boj/p16k/p16700/P16724.java
+++ src/main/java/boj/p16k/p16700/P16724.java
@@ -1,6 +1,6 @@
 package boj.p16k.p16700;
 
-
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,50 +8,59 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.function.IntBinaryOperator;
 
 public class P16724 {
-	static int N, M;
-	static int[][] di = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } }; // 상하좌우
-	static int[][] map, check;
-	static int cnt = 0;
-	static boolean[][] visited;
+    static int N, M;
+    static int[][] di = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우
+    static int[][] map, check;
+    static int cnt = 0;
+    static boolean[][] visited;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
-		check = new int[N][M];
-		visited = new boolean[N][M];
 
-		for (int i = 0; i < N; i++) {
-			String line = in.readLine();
-			for (int j = 0; j < M; j++) {
-				char cur = line.charAt(j);
-				map[i][j] = cur == 'L' ? 2 : cur == 'R' ? 3 : cur == 'U' ? 0 : 1;
-			}
-			Arrays.fill(check[i], -1);
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        check = new int[N][M];
+        visited = new boolean[N][M];
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (visited[i][j])
-					continue;
-				search(i, j);
-			}
-		}
-		System.out.print(cnt);
 
-	}
+        for (int i = 0; i < N; i++) {
+            String line = in.readLine();
+            for (int j = 0; j < M; j++) {
+                char cur = line.charAt(j);
+                map[i][j] = cur == 'L' ? 2 : cur == 'R' ? 3 : cur == 'U' ? 0 : 1;
+            }
+            Arrays.fill(check[i], -1);
+        }
 
-	public static int search(int x, int y) {
-		if (check[x][y] != -1)
-			return check[x][y];
-		if (x < 0 || y < 0 || N <= x || M <= y || visited[x][y])
-			return cnt++;
-		visited[x][y] = true;
-		return check[x][y] = search(x + di[map[x][y]][0], x + di[map[x][y]][1]);
-	}
+     
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+            	if (visited[i][j]) continue;
+            	search(i, j);
+            }
+        }
+        System.out.print(cnt);
+        
+        
+        
+        
 
+    
+}
+    
+    
+    public static int search(int x, int y) {
+    	if (check[x][y] != -1 ) return check[x][y];
+        if (x < 0 || y < 0 || N <= x || M <= y || visited[x][y])  return cnt ++;
+        visited[x][y] = true;
+        return check[x][y]=search(x + di[map[x][y]][0], y + di[map[x][y]][1]);        	
+    }
+    
+    
 }
