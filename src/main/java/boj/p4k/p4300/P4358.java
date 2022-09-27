cc9c381fa7822411ba1281de0f5ab66c07f05e17
xfrnk2 <xfrnk2@gmail.com>
2022-09-27 22:09:21 +0900

    solve: 4358 생태학

---
+++ src/main/java/boj/p4k/p4300/P4358.java
@@ -0,0 +1,96 @@
+package boj.p4k.p4300;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class P4358 {
+
+	static int total = 0;
+	static List<Res> result = new ArrayList<>();
+
+	static class Node {
+		int cnt;
+		Node[] child;
+
+		public Node() {
+			cnt = 0;
+			child = new Node[96]; // 128 - 32(" ")
+		}
+	}
+
+	static class Res implements Comparable<Res> {
+		String str;
+		float rate;
+
+		public Res(String str, float f) {
+			super();
+			this.str = str;
+			this.rate = f;
+		}
+
+		public int compareTo(Res o) {
+			return str.compareTo(o.str);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str = null;
+		Node root = new Node();
+		while (true) {
+			str = in.readLine();
+			if (str == null || str.equals("")) {
+				break;
+			}
+			total++;
+			add(root, str, 0, str.length());
+		}
+		search(root, new StringBuilder());
+
+		Collections.sort(result, null);
+		for (Res elem : result) {
+			out.write(String.format("%s %.4f\n", elem.str, elem.rate));
+		}
+		out.flush();
+		out.close();
+
+	}
+
+	private static void add(Node node, String str, int idx, int strSize) {
+		if (idx == strSize) {
+			node.cnt++;
+			return;
+		}
+		int cur = str.charAt(idx) - 32;
+		if (node.child[cur] == null) {
+			node.child[cur] = new Node();
+			add(node.child[cur], str, idx + 1, strSize);
+			return;
+		}
+		add(node.child[cur], str, idx + 1, strSize);
+	}
+
+	private static void search(Node node, StringBuilder sb) {
+		if (node.cnt != 0) { // 트리 내에서 문자열을 찾은 경우
+			result.add(new Res(sb.toString(), (float) ((float) node.cnt / (float) total) * 100));
+		}
+
+		for (int j = 0; j < 96; j++) {
+			int i = j + 32;
+			if (node.child[j] != null) {
+				sb.append((char) i);
+				search(node.child[j], sb);
+				sb.deleteCharAt(sb.length() - 1);
+			}
+		}
+
+	}
+
+}
