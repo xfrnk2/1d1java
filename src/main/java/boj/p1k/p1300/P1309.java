6509ea2ab3d4d31e4e1e654585f07b264c3bec0f
xfrnk2 <xfrnk2@gmail.com>
2022-10-05 23:10:07 +0900

    refactor: 1309 정답 코드로 변경

--- src/main/java/boj/p1k/p1300/P1309.java
+++ src/main/java/boj/p1k/p1300/P1309.java
@@ -8,16 +8,18 @@ public class P1309 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int m = 9901;
 		int[][] d = new int[n+1][3];
 		
 		d[1][0] = d[1][1] = d[1][2] = 1;
 		
 		for (int i = 2; i <= n; i++) {
-			d[i][0] = d[i-1][0] + d[i-1][1] + d[i-1][2];
-			d[i][1] = d[i-1][0] + d[i-1][2];
-			d[i][2] = d[i-1][0] + d[i-1][1];
+			d[i][0] = (d[i-1][0] + d[i-1][1] + d[i-1][2]) % m;
+			d[i][1] = (d[i-1][0] + d[i-1][2]) % m;
+			d[i][2] = (d[i-1][0] + d[i-1][1]) % m;
+			
 		}
-		System.out.println(d[n][0] + d[n][1] + d[n][2]);
+		System.out.println((d[n][0] + d[n][1] + d[n][2]) % m) ;
 	}
 
 }
