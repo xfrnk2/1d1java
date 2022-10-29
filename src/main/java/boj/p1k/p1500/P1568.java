2a83f853e4d7d4cb1cbab8fe8e9992fe03a464ac
xfrnk2 <xfrnk2@gmail.com>
2022-10-29 22:59:42 +0900

    solve: 1568 새

---
+++ src/main/java/boj/p1k/p1500/P1568.java
@@ -0,0 +1,24 @@
+package boj.p1k.p1500;
+
+import java.util.*;
+
+public class P1568 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int bird = 1;
+        int count = 0;
+
+        while (n >= 1) {
+            if (n < bird) {
+                bird = 1;
+            }
+            n -= bird;
+            bird++;
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
