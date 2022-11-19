833621e82e0dd718e7fc3338d07a1c5fc29b9df2
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2022-11-19 13:50:06 +0900

    solve: 2386 도비의 영어공부

---
+++ src/main/java/boj/p2k/p2300/P2386.java
@@ -0,0 +1,25 @@
+package boj.p2k.p2300;
+
+import java.util.Scanner;
+
+public class P2386 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			String N = sc.nextLine();
+
+			if (N.equals("#"))
+				System.exit(0);
+
+			String strArr[] = N.toLowerCase().split("");
+
+			int count = 0;
+			for (int i = 0; i < strArr.length; i++) {
+				if (N.split(" ")[0].equals(strArr[i]))
+					count++;
+			}
+			System.out.println(N.split(" ")[0] + " " + (count - 1));
+		}
+	}
+}
