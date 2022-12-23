ae3379883180e6adf7188adb5d94927e187cc2d4
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2022-12-23 23:14:03 +0900

    solve: 1550 16진수

---
+++ src/main/java/boj/p1k/p1500/P1550.java
@@ -0,0 +1,13 @@
+package boj.p1k.p1500;
+
+
+import java.util.Scanner;
+
+public class P1550 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		int ans = Integer.parseInt(line, 16);
+		System.out.println(ans);
+	}
+}
