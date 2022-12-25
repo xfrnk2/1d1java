0e7070d857fd94d8be631ef3fd78d28c81c1dc1f
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2022-12-26 01:09:20 +0900

    solve: 10775 공항

---
+++ src/main/java/boj/p10k/p10700/P10775.java
@@ -0,0 +1,44 @@
+package boj.p10k.p10700;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class P10775 {
+	static int G, P, ans = 0;
+	static int[] visit;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		G = Integer.valueOf(in.readLine());
+		P = Integer.valueOf(in.readLine());
+		visit = new int[G + 1];
+		int start = 1;
+		boolean finish = true;
+		for (int i = 0; i < P; i++) {
+			int target = Integer.valueOf(in.readLine());
+
+			while (target > 0 && visit[target] > 0) {
+				
+				visit[target]++;
+				target-= visit[target] - 1;
+			}
+
+			if (target <= 0) {
+				break;
+			} else {
+				visit[target] = 1;
+				ans ++;
+			}
+
+
+		}
+		System.out.println(ans);
+	}
+
+}
