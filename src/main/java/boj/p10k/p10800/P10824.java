18959cc0ec73f96326335890e92ddeb24a4a5829
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-01-21 23:52:52 +0900

    solve: 10824 네 수

---
+++ src/main/java/boj/p10k/p10800/P10824.java
@@ -0,0 +1,20 @@
+package boj.p10k.p10800;
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P10824 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        String ab = st.nextToken() + st.nextToken();
+        String cd = st.nextToken() + st.nextToken();
+
+        System.out.println(Long.parseLong(ab)+Long.parseLong(cd));
+    }
+}
