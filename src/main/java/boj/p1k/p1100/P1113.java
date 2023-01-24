2326f9ead215908fff43432fe1282a13e625e989
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-01-25 01:39:49 +0900

    refactor: 1113 수영장 만들기

--- src/main/java/boj/p1k/p1100/P1113.java
+++ src/main/java/boj/p1k/p1100/P1113.java
@@ -1,78 +1,99 @@
 package boj.p1k.p1100;
 
 
-import java.io.*;
-import java.util.*;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class P1113 {
-	static int map[][], N, M, min;
-	static boolean visited[][];
-	static List<int[]> nodes;
-	
-	public static void dfs(int x, int y, int n) {
-		nodes.add(new int[] {x, y});
-		int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
-		for(int i=0; i<4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if(map[nx][ny]!=0) {
-				if(map[nx][ny]!=n) {
-					min = Math.min(min, map[nx][ny]);
-					continue;
-				}
-				if(visited[nx][ny]) {continue;}
-				visited[nx][ny] = true;
-				dfs(nx, ny, n);
-			}
-			else {
-				min = 0;
+	static int N, M;
+	static int[][] map;
+	static int[][] di = new int[][] { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		N = Integer.valueOf(st.nextToken());
+		M = Integer.valueOf(st.nextToken());
+		map = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			String input = in.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = input.charAt(j) - '0';
 			}
 		}
+		int ans = 0;
+		for (int k = 1; k < 10; k++) {
+			int res = search(k);
+			ans += res;
+		}
+		System.out.println(ans);
 	}
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[N+2][M+2];
-		visited = new boolean[N+2][M+2];
-		int result = 0;
-		for(int i=1; i<N+1; i++) {
-			map[i][0] = 0;
-			map[i][M+1] = 0;
-			String s = br.readLine();
-			for(int j=1; j<M+1; j++) {
-				map[i][j] = s.charAt(j-1)-'0';
-			}
-		}
-		//벽세우기
-		for(int i=0; i<M+2; i++) {
-			map[0][i] = 0;
-			map[N+1][i] = 0;
-		}
-		
-		for(int i=1; i<10; i++) {
-			//전체를 돌면서
-			for(int j=1; j<N+1; j++) {
-				for(int k=1; k<M+1; k++) {
-					if(map[j][k]==i && !visited[j][k]) {
-						nodes = new LinkedList<>();
-						min = 9;//최대 9까지 가능
-						visited[j][k] = true;
-						dfs(j, k, i);
-						if(min>i) {
-							int temp = min-i;
-							for(int[] pos : nodes) {
-								map[pos[0]][pos[1]] = min;
-								visited[pos[0]][pos[1]]=false;
-								result += temp;
+	public static boolean isOut(int x, int y) {
+		return x < 0 || y < 0 || N <= x || M <= y;
+	}
+
+	public static int search(int height) {
+		boolean[][] visit = new boolean[N][M];
+		int res = 0;
+
+		for (int i = 1; i < N - 1; i++) {
+			for (int j = 1; j < M - 1; j++) {
+
+				if (!visit[i][j] && height == map[i][j]) {
+
+					Set<Point> record = new HashSet<>();
+					Queue<Point> q = new LinkedList<>();
+					visit[i][j] = true;
+					record.add(new Point(i, j));
+					q.add(new Point(i, j));
+
+					int max = 9;
+					while (!q.isEmpty()) {
+						Point cur = q.poll();
+						record.add(new Point(cur.x, cur.y));
+						for (int k = 0; k < 4; k++) {
+							int nx = cur.x + di[k][0], ny = cur.y + di[k][1];
+
+							if (!isOut(nx, ny)) {
+
+								if (map[nx][ny] != height) {
+									max = Math.min(max, map[nx][ny]);
+									continue;
+								}
+								if (visit[nx][ny])
+									continue;
+								visit[nx][ny] = true;
+								q.add(new Point(nx, ny));
+
+							} else {
+								max = 0;
 							}
+
 						}
 					}
+					if (height < max) {
+						int interval = max - height;
+						for (Point point : record) {
+							map[point.x][point.y] = max;
+							visit[point.x][point.y] = false;
+							res += interval;
+						}
+					}
+
 				}
 			}
 		}
-		System.out.println(result);
+		return res;
 	}
+
 }
