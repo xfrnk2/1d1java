59157991ab7666686718720f48825df34af781c3
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-02-02 00:06:20 +0900

    solve: 2441 별 찍기 - 4

---
+++ src/main/java/boj/p2k/p2400/P2441.java
@@ -0,0 +1,25 @@
+package boj.p2k.p2400;
+
+
+import java.util.Scanner;
+
+public class P2441 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<i; j++) {
+				System.out.print(" ");
+			}
+			for (int j=n; j>=i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
