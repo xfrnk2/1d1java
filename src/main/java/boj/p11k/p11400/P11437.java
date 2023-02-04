7e7eb25d604517591d775867d28c381f482c051f
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-02-04 23:04:58 +0900

    fix: 11437 LCA

--- src/main/java/boj/p11k/p11400/P11437.java
+++ src/main/java/boj/p11k/p11400/P11437.java
@@ -1,10 +1,13 @@
+package boj.p11k.p11400;
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class P11437 {
 	static int N, M;
 	static ArrayList<Integer>[] tree;
 	static int[] depths, parents;
