0de9f9c35989e5e272060bbe46ecd2ae8afc4295
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-03-02 21:50:15 +0900

    solve: 3046 R2

---
+++ src/main/java/boj/p3k/p3000/P3046.java
@@ -0,0 +1,23 @@
+package boj.p3k.p3000;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P3046 {
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	public static void main(String[] args) throws IOException { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int R1 = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		
+		bw.write(String.valueOf(2*S-R1));
+		bw.flush(); 
+		bw.close();
+	}
+}
