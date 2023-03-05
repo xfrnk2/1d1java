5d3c600858181812e17ecfedee4bcbc5c47d121a
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-03-05 18:00:01 +0900

    solve: 10039 평균 점수

---
+++ src/main/java/boj/p10k/p10000/P10039.java
@@ -0,0 +1,21 @@
+package boj.p10k.p10000;
+
+
+import java.util.Scanner;
+
+
+public class P10039 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int score;
+        int sum=0;
+        for(int i=0; i<5; i++){
+            score=sc.nextInt();
+            if(score<40) {
+              score=40;  
+            } 
+            sum += score;
+        }
+        System.out.println(sum/5);
+    }
+}
