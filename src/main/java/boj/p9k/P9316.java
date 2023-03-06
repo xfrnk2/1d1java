7660cbc14110c8ce6c8f248671f0fb6b845506e9
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-03-06 21:30:32 +0900

    solve: 9316 Hello Judge

---
+++ src/main/java/boj/p9k/P9316.java
@@ -0,0 +1,17 @@
+package boj.p9k.P9316;
+
+
+import java.util.Scanner;
+
+public class P9316 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			System.out.println("Hello World, Judge " + i + "!");
+		}
+	}
+
+}
