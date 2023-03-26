7703a6cfdba74e087145cf8bf390f410da3230b1
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-03-26 21:40:11 +0900

    solve: 2752 세수 정렬

---
+++ src/main/java/boj/p2k/p2700/P2752.java
@@ -0,0 +1,22 @@
+package boj.p2k.p2700;
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class P2752 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3]; 
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()); 
+        }
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
