eb7ba39a95fbda4414c1bae83b53607f4dffb835
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-03-31 21:12:25 +0900

    solve: 11943 파일 옮기기

---
+++ src/main/java/boj/p11k/p11900/P11943.java
@@ -0,0 +1,20 @@
+package boj.p11k.p11900;
+
+
+import java.util.Scanner;
+
+public class P11943 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+
+		System.out.println(Math.min(A + D, B + C));
+
+	}
+
+}
