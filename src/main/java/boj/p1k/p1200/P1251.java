25c6416b5e26958cc610bb85f8095df6aaf39523
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-04-12 23:15:56 +0900

    solve: 1251 단어 나누기

---
+++ src/main/java/boj/p1k/p1200/P1251.java
@@ -0,0 +1,36 @@
+package boj.p1k.p1200;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class P1251 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder(in.readLine());
+		PriorityQueue<String> pq = new PriorityQueue<>();
+
+		String ans = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
+
+		int size = sb.length();
+		for (int i = 1; i < size - 1; i++) {
+			for (int j = i; j < size - 1; j++) {
+
+				StringBuilder a = new StringBuilder(sb.substring(0, i));
+				StringBuilder b = new StringBuilder(sb.substring(i, j + 1));
+				StringBuilder c = new StringBuilder(sb.substring(j + 1, size));
+
+				String s = a.reverse() + "" + b.reverse() + "" + c.reverse();
+
+				if (ans.compareTo(s) > 0) {
+					ans = s;
+				}
+
+			}
+		}
+		System.out.println(ans);
+
+	}
+}
