c5264d70997d2d9a196130f2e0aa08f9750b5351
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-04-20 00:16:46 +0900

    solve: 15439 Vera And Outfits

---
+++ src/main/java/boj/p15k/p15400/P15439.java
@@ -0,0 +1,16 @@
+package boj.p15k.p15400;
+
+
+import java.util.Scanner;
+
+public class P15439 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		
+		System.out.println(N * (N - 1));
+	}
+
+}
