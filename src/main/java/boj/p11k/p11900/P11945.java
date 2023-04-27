6450aebb7fbef9abbfa43f210a06d2fc6dc723e0
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-04-27 23:03:07 +0900

    solve: 11945 뜨거운 붕어빵

---
+++ src/main/java/boj/p11k/p11900/P11945.java
@@ -0,0 +1,22 @@
+package boj.p11k.p11900;
+
+
+
+import java.util.Scanner;
+
+public class P11945 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+
+		for (int i = 0; i < x; i++) {
+			while (sc.hasNext()) {
+				StringBuilder sb = new StringBuilder(sc.next());
+				System.out.println(sb.reverse());
+			}
+		}
+	}
+
+}
