e8cd9ec6cd165d83bab39f7b015a2ec4534c5e07
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-05-01 22:53:38 +0900

    solve: 15733 나는 누구인가?

---
+++ src/main/java/boj/p15k/p15700/P15733.java
@@ -0,0 +1,11 @@
+package boj.p15k.p15700;
+
+
+import java.io.*;
+import java.util.*;
+
+public class P15733 {
+    public static void main(String[] args) throws IOException {
+        System.out.print("I'm Sexy");
+    }
+}
