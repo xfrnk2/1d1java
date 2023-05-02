5cddc20ee9e36d55e1294486e3188a04b0c0a864
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-05-02 23:44:27 +0900

    solve: 10569 다면체

---
+++ src/main/java/boj/p10k/p10500/P10569.java
@@ -0,0 +1,13 @@
+package boj.p10k.p10500;
+
+
+import java.util.*;
+
+public class P10569 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+			System.out.println(2-sc.nextInt()+sc.nextInt());
+	}
+}
