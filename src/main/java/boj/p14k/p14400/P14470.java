894887ab15522be6dc2a534e207c3236751b1b56
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-05-16 23:49:16 +0900

    solve: 14470 전자레인지

---
+++ src/main/java/boj/p14k/p14400/P14470.java
@@ -0,0 +1,25 @@
+package boj.p14k.p14400;
+
+
+import java.util.Scanner;
+
+public class P14470 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		int E = sc.nextInt();
+
+		int sec = 0;
+
+		if (A < 0) {
+			sec = (A * C * -1) + (B * E) + D;
+		} else {
+			sec = (B - A) * E;
+		}
+		System.out.println(sec);
+	}
+}
