870daf6ecd431451d8860f725ece1f7fef9058cf
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-05-19 00:18:53 +0900

    solve: 15829 hashing

---
+++ src/main/java/boj/p15k/p15800/P15829.java
@@ -0,0 +1,23 @@
+package boj.p15k.p15800;
+
+
+import java.util.*;
+import java.io.*;
+
+class P15829 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+        int r = 31;
+        int m = 1234567891;
+        long sum = 0;
+        long mod = 1;
+        char[] ch = br.readLine().toCharArray();
+
+        for(int i = 0; i < length; i++) {
+
+            sum += (((int)((ch[i] - 'a') + 1)) * mod);
+            mod = (r * mod) % m;
+        }
+        System.out.println(sum % m);
+    }
