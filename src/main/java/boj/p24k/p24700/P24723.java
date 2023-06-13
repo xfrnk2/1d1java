0b307b673bb3f76d09457ee679179cd52c017418
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-06-13 23:06:37 +0900

    solve: 24723 녹색 거탑

---
+++ src/main/java/boj/p24k/p24700/P24723.java
@@ -0,0 +1,13 @@
+package boj.p24k.p24700;
+
+import java.util.*;
+
+
+public class P24723 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        System.out.println((int)Math.pow(2,N));
+    }
+}
