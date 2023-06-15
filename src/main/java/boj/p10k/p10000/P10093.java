3cd4fd0cbbb93cd3fad3e9b2a23fb1c30e474bdf
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-06-15 23:11:17 +0900

    solve: 10093 숫자

---
+++ src/main/java/boj/p10k/p10000/P10093.java
@@ -0,0 +1,24 @@
+package boj.p10k.p10000;
+
+
+import java.util.*;
+
+
+public class P10093 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        if(b>a) {
+            System.out.println(b-a-1);
+            for(long i = a+1; i<b; i++) System.out.print(i+" ");
+        }
+        else if(a>b) {
+            System.out.println(a-b-1);
+            for(long i = b+1; i<a; i++) System.out.print(i+" ");
+        }
+        else {
+            System.out.print(0);
+            }
+    }
+}
