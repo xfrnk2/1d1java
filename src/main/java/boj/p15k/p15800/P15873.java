07d7c4dc1b4beb015d6947c1dedefbdae0d98d43
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-07-10 21:20:21 +0900

    solve: 15873 공백 없는 A+B

---
+++ src/main/java/boj/p15k/p15800/P15873.java
@@ -0,0 +1,27 @@
+package boj.p15k.15800;
+
+
+import java.util.Scanner;
+
+
+public class P15873 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        
+        a = sc.nextInt();
+        b = 0;
+        
+        if (a == 1010){
+            System.out.println(20);
+        }
+        else if (a % 10 == 0){
+            a /= 100;
+            System.out.println(a+10);
+        } else {
+            b += a % 10;
+            a /= 10;
+            System.out.println(a == 10 ? 10 + b : a + b);
+        }
+    }
+}
