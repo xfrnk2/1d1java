26d0652df687266ec57cf4ec304254cfcb281339
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-07-21 17:22:09 +0900

    solve: 10773 제로

---
+++ src/main/java/boj/p10k/p10700/P10773.java
@@ -0,0 +1,33 @@
+package boj.p10k.p10700;
+
+
+import java.util.*;
+import java.io.*;
+
+
+public class P10773 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		int K = Integer.parseInt(in.readLine());
+		Stack<Integer> stack = new Stack<>();
+
+		for(int i=0; i<K; i++) {
+			int input = Integer.parseInt(br.readLine());
+
+			if(input == 0) {
+				stack.pop();
+			}
+			else {
+				stack.push(input);
+			}
+		}
+
+		int ans = 0;
+		while (!stack.isEmpty()) {
+			ans += stack.pop();
+		}
+
+		System.out.println(ans);
+	}
+}
