1d21900d2d66b28259dfb031ec799b46187e8985
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-07-23 22:55:12 +0900

    solve: 15831 준표의 조약돌

---
+++ src/main/java/boj/p15k/p15800/P15831.java
@@ -0,0 +1,41 @@
+package boj.p15k.15800;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15831 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int max = 0;
+		int wNum = 0;
+		int bNum = 0;
+		String input = in.readLine();
+		for (int i = 0; i < N; i++) {
+			if (input.charAt(i) == 'W') wNum++;
+			else {
+				bNum++;
+				while (B < bNum) {
+					if (input.charAt(i + 1 - (bNum + wNum)) == 'W') {
+						wNum--;
+					} else {
+						bNum--;
+					}
+				}
+			}
+			if (W <= wNum && max < wNum + bNum) {
+				max = Math.max(max, wNum + bNum);
+			}
+		}
+		System.out.println(max);
+	}
+
+}
